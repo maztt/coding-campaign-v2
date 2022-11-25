ba8580098283f06c2d7063a4aec4e48b45dc3280
Júlio Mazotti <92126792+maztt@users.noreply.github.com>
2022-11-25 16:10:04 -0300

    feat: building pages, functions and styles

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # coding-campaign-v2
-This is a Work In Progress
+
+This is a Work In Progress, improving the project [Coding Campaign](https://github.com/maztt/coding-campaign) to a new concept, style, function and others.
+
+
+Original by me.
---
+++ src/js/page-newgame.js
@@ -0,0 +1,68 @@
+window.alert('Maximize your experience by using the keyboard.')
+
+function playMenuSound() {
+  const menuSound = new Audio('src/audios/menu-sound-effect.mp4');
+  menuSound.play();
+}
+
+const option_NewGame = document.getElementById('options-1')
+const option_Options = document.getElementById('options-2')
+
+
+document.onkeydown = function(e) {
+
+const event = e.key
+
+switch (event) {
+    case 'ArrowUp':
+        if (option_NewGame.attributes.class.value == 'selected') {
+          console.log('pra cima')
+        } else {
+          option_Options.classList.toggle('selected');
+          option_NewGame.classList.toggle('selected');
+          playMenuSound();
+        }             
+        break;
+    case 'ArrowDown':
+        if (option_Options.attributes.class.value  == 'selected') {
+          break;
+        } else {
+          option_NewGame.classList.toggle('selected');
+          option_Options.classList.toggle('selected');
+          playMenuSound();
+        }             
+        break;
+    case 'Enter':
+        console.log('enter')
+        if (option_NewGame.classList.contains('selected')) {
+          console.log('newgame: esta opção')
+          newGame()
+        } else {
+          console.log('options: esta opção')
+          option()
+        }
+        playMenuSound();
+      break;
+    case 'Spacebar':
+        console.log('espaço');
+        if (option_NewGame.classList.contains('selected')) {
+          console.log('newgame: esta opção')
+        } else {
+          console.log('options: esta opção')
+          option()
+        }
+        playMenuSound();
+        break;
+}
+
+}
+
+function newGame() {
+  const newgame = document.getElementById('newgame')
+  setTimeout(newgame.click(), 2000)
+}
+
+function option() {
+  const options = document.getElementById('options')
+  setTimeout(options.click(), 2000)
+}
