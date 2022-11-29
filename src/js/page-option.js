00810b078a9efc17c7784cc73ea41760cf56c3ff
Júlio Mazotti <92126792+maztt@users.noreply.github.com>
2022-11-29 11:47:56 -0300

    feat: add option selection

--- src/js/page-option.js
+++ src/js/page-option.js
@@ -7,23 +7,34 @@ const option_Multiplayer = document.getElementById('option_multiplayer')
 const option_Language = document.getElementById('option_language')
 const option_Exit = document.getElementById('option_exit')
 
+const action_Exit = document.getElementById('i-exit')
+
 const options = [option_Productivity, option_studying, option_Listening, option_ToHave, option_Battle, option_Multiplayer, option_Language, option_Exit];
 
 let optionIndex = 0;
+let itemIndex = 0;
 
 function playMenuSound() {
   const menuSound = new Audio('src/audios/menu-sound-effect.mp4');
   menuSound.play();
 }
 
+
+function exit() {
+  setTimeout(action_Exit.click(), 2000)
+}
+
+
 document.onkeydown = function(e) {
   const event = e.key
 
   switch(event) {
+
     case 'ArrowUp':
       if (optionIndex == 0) {
         return
       } else {
+        itemIndex = 0;
         optionIndex--
         options[optionIndex + 1].classList.remove('selected')
         options[optionIndex].classList.add('selected')
@@ -31,12 +42,13 @@ document.onkeydown = function(e) {
         optionEnable()
         playMenuSound()
       }
-      console.log(optionIndex)
       break
+      
     case 'ArrowDown':
       if (optionIndex == 7) {
         return
       } else {
+        itemIndex = 0;
         optionIndex++
         options[optionIndex - 1].classList.remove('selected')
         options[optionIndex].classList.add('selected')
@@ -44,13 +56,41 @@ document.onkeydown = function(e) {
         optionEnable()
         playMenuSound()
       }
-      console.log(optionIndex)
       break
+
+    case 'ArrowRight':
+      items = options[optionIndex].nextElementSibling.firstElementChild.children[1].children
+      if (itemIndex >= items.length - 1) {
+        return
+      } else {
+        itemIndex++
+        items[itemIndex].classList.remove('dontdisplay')
+        items[itemIndex - 1].classList.add('dontdisplay')
+        playMenuSound()
+      }
+      break
+
+    case 'ArrowLeft':
+      items = options[optionIndex].nextElementSibling.firstElementChild.children[1].children
+      if (itemIndex == 0) {
+        return
+      } else {
+        itemIndex--
+        items[itemIndex].classList.remove('dontdisplay')
+        items[itemIndex + 1].classList.add('dontdisplay')
+        playMenuSound()
+      }
+      break
+
+      case 'Enter':
+        if (option_Exit.classList.contains('selected')) {
+          exit()
+        }
   }
 }
 
 
-const action_Exit = document.getElementById('i-exit')
+
 const options_group = document.getElementsByClassName('options_group')
 
 const optionEnable = () => {
@@ -74,6 +114,3 @@ const optionDisable = () => {
   }
 
 }
-  
-  // options_group.forEach((group) => {
-  // })
