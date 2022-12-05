e9037213fae83a5b3c618aefbdaa67c877b2adb2
Júlio Mazotti <92126792+maztt@users.noreply.github.com>
2022-12-05 20:27:47 -0300

    feat: add typing effect style with interactions

--- src/js/page-intro.js
+++ src/js/page-intro.js
@@ -1,54 +1,68 @@
 const professorOak = [
   "Hello there! Glad to meet you, welcome to the World of Coding!",
   "My name is OAK! People call me the Coding Prof!",
-  "This world is inhabited by creatures called POKEMÓN! For some people, pokémon are pets.",
-  "Others use them for fights. Myself... I study Coding Languages as a profession.",
-  "Today I'll be guiding you the best way I can so you can begin your journey the best way you can",
+  "This world is inhabited by creatures called CODING LANGUAGES!",
+  "For some people, coding languages are a way of doing great things.",
+  "Others use them for arguing. Myself... I study them as a profession.",
+  "Today I'll be guiding you the best way I can so you can also begin your journey the best way possible.",
   "Firstly, don't be afraid. There will be a lot of information but you'll get used to it.",
+  "I'll introduce you to some paths, look inside yourself to see what's the best fit.",
   "Now tell me, after checking the descriptions choose one of the alternatives.",
   "Wow! That's a really great choice!",
-  "Now come with me, I'll show you many options from which you can pick a main language.",
-  "for your main campaign and also one more so you can have fun with the secondary quests",
+  "These work as a filter for us to know what and how you like to do things.",
+  "Now come with me, I'll show you many options from which you can make some picks.",
+  "These picks will be your coding campaign, which one will be the one you'll have to focus the most and",
+  "also one more so you can have fun with the secondary quests, explore new worlds and such!",
   "But here's a big reminder! Listen close!",
-  "This will not define your whole career, it's okay to experience them until you pick the one you most like",
+  "This will not define your whole career, it's okay to experience them until you choose the one you most like",
   "And feel comfortable dealing with, okay? You have to like it!",
   "Sure, I see you there nodding positively.",
   "Let's check our candidates."
 ];
 
 const message = document.getElementById("typedtext");
-const textSpeed = 40;
+const textSpeed = 30;
 
 let i = 0;
 let count = 0;
+let isActive = false;
 
 document.onkeydown = function(e) {
   const event = e.key
   
   switch(event) {
     case 'Enter':
-      playMenuSound()
-      clearChat();
-      chat();
+      chat()
     }
-    
   }
   
   
-  function clearChat() {
-      message.innerHTML = '';
+function clearChat() {
+    message.innerHTML = '';
+}
+  
+async function chat() {
+  if (isActive === true) {
+    return
+  } else {
+    playMenuSound()
+    clearChat();
+    await chatWriter()
   }
-  
-  function chat() {
-  
+}
 
-    if (i < professorOak[count].length) {
-      message.innerHTML += professorOak[count].charAt(i);
-      i++;
-      setTimeout(chat, textSpeed);
-    } else {
-      i = 0;
-      count++
+async function chatWriter() {
+
+  isActive = true;
+
+  if (i < professorOak[count].length) {
+    message.innerHTML += professorOak[count].charAt(i);
+    i++;
+    setTimeout(chatWriter, textSpeed);
+  } else {
+    i = 0;
+    count++
+    isActive = false
   }
 
 }
