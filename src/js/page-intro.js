4d89ce5d01e0798a4abfc4ee1abe236577febd2b
Júlio Mazotti <92126792+maztt@users.noreply.github.com>
2022-12-04 22:23:17 -0300

    trying to fix/improve the algorithm

--- src/js/page-intro.js
+++ src/js/page-intro.js
@@ -1,4 +1,4 @@
-const firstGroup = [
+const professorOak = [
   "Hello there! Glad to meet you, welcome to the World of Coding!",
   "My name is OAK! People call me the Coding Prof!",
   "This world is inhabited by creatures called POKEMÓN! For some people, pokémon are pets.",
@@ -6,9 +6,6 @@ const firstGroup = [
   "Today I'll be guiding you the best way I can so you can begin your journey the best way you can",
   "Firstly, don't be afraid. There will be a lot of information but you'll get used to it.",
   "Now tell me, after checking the descriptions choose one of the alternatives.",
-];
-
-const secondGroup = [
   "Wow! That's a really great choice!",
   "Now come with me, I'll show you many options from which you can pick a main language.",
   "for your main campaign and also one more so you can have fun with the secondary quests",
@@ -17,72 +14,46 @@ const secondGroup = [
   "And feel comfortable dealing with, okay? You have to like it!",
   "Sure, I see you there nodding positively.",
   "Let's check our candidates."
-]
+];
 
 const message = document.getElementById("typedtext");
 const textSpeed = 40;
-const textStart = 0;
-const textLength = firstGroup[0].length;
-const scrollAt = 20;
 
-let content = '';
-
+let i = 0;
+let count = 0;
 
 document.onkeydown = function(e) {
   const event = e.key
-
+  
   switch(event) {
     case 'Enter':
+      playMenuSound()
+      clearChat();
       chat();
-  }
-}
-
-  var iSpeed = 40; // time delay of print out
-  var iIndex = 0; // start printing array at this posision
-  var iArrLength = firstGroup[0].length; // the length of the text array
-  var iScrollAt = 20; // start scrolling up at this many lines
-   
-  var iTextPos = 0; // initialise text position
-  var sContents = ''; // initialise contents variable
-  var iRow; // initialise current row
-
-var i = 0;
-var txt = 'Lorem ipsum typing effect!'; /* The text */
-var speed = 30;
-   
-  function chat() {
-
-    if (i < txt.length) {
-      message.innerHTML += txt.charAt(i);
-      i++;
-      setTimeout(chat, speed);
     }
-  }
-
-  function typewriter() {
-
-   iRow = Math.max(0, iIndex-iScrollAt);
-   var destination = document.getElementById("typedtext");
-   
-   while ( iRow < iIndex ) {
-    sContents = firstGroup[iRow++] + '<br />';
-   }
-
-   destination.innerHTML = sContents + firstGroup[iIndex].substring(0, iTextPos) + "|";
-   if ( iTextPos++ == iArrLength ) {
-    iTextPos = 0;
-    iIndex++;
-    if ( iIndex != firstGroup.length ) {
-     iArrLength = firstGroup[iIndex].length;
-     setTimeout("typewriter()", 500);
-    }
-   } else {
-    setTimeout("typewriter()", iSpeed);
-   }
-
+    
   }
   
-  function playMenuSound() {
-    const menuSound = new Audio('src/audios/menu-sound-effect.mp4');
-    menuSound.play();
+  
+  function clearChat() {
+      message.innerHTML = '';
   }
+  
+  function chat() {
+  
+
+    if (i < professorOak[count].length) {
+      message.innerHTML += professorOak[count].charAt(i);
+      i++;
+      setTimeout(chat, textSpeed);
+    } else {
+      i = 0;
+      count++
+  }
+
+}
+
+function playMenuSound() {
+  const menuSound = new Audio('src/audios/menu-sound-effect.mp4');
+  menuSound.play();
+}
