f2bc56cec4422a70c62e00e281b6333c71ebeb50
Júlio Mazotti <92126792+maztt@users.noreply.github.com>
2023-05-11 15:30:00 -0300

    refactor: improve code clarity and sound performance while navigating

--- src/js/page-index.js
+++ src/js/page-index.js
@@ -1,47 +1,63 @@
-window.alert('Maximize your experience by using the keyboard.')
-
-function playMenuSound () {
-  const menuSound = new Audio('./src/audios/menu-sound-effect.mp4');
-  menuSound.play();
+const playMenuSound = () => {
+  const menuSound = new Audio('./src/audios/menu-sound-effect.mp4')
+  menuSound.play()
 }
 
-function newGame() {
-  const newgame = document.getElementById('newgame')
-  setTimeout(newgame.click(), 2000)
+const navigateToNewGame = () => {
+  const newGame = document.getElementById('newgame')
+  setTimeout(() => {
+    newGame.click(), 2000
+  })
 }
 
-function option() {
+const navigateToOptions = () => {
   const options = document.getElementById('options')
-  setTimeout(options.click(), 2000)
+  setTimeout(() => {
+    options.click(), 2000
+  })
 }
-
-window.alert('Maximize your experience by using the keyboard.')
 
 const option_NewGame = document.getElementById('option-newgame')
 const option_Options = document.getElementById('option-options')
 
+
+// In case the user is using keyboard
 document.onkeydown = function(e) {
-
   const event = e.key
-
   switch (event) {
-      case 'ArrowUp':
-          option_Options.classList.toggle('selected');
-          option_NewGame.classList.toggle('selected');
-          playMenuSound();       
-          break;
-      case 'ArrowDown':
-          option_NewGame.classList.toggle('selected');
-          option_Options.classList.toggle('selected');
-          playMenuSound();
-          break;
-      case 'Enter':
-          playMenuSound();
-          if (option_NewGame.classList.contains('selected')) {
-            setTimeout(newGame(), 1000)
-          } else {
-            setTimeout(option(), 1000)
-          }
-        break;
+    case 'ArrowUp':
+        playMenuSound()
+        option_Options.classList.toggle('selected')
+        option_NewGame.classList.toggle('selected')
+        break
+    case 'ArrowDown':
+        playMenuSound()
+        option_NewGame.classList.toggle('selected')
+        option_Options.classList.toggle('selected')
+        break
+    case 'Enter':
+        if (option_NewGame.classList.contains('selected')) {
+          navigateToNewGame()
+        } else {
+          navigateToOptions()
+        }
+        break
     }
 }
+
+// In case the user is using mobile or mouse
+option_NewGame.addEventListener('click', (e) => {
+  playMenuSound()
+  e.preventDefault()
+  setTimeout(() => {
+    window.location.href = 'src/pages/newgame.html';
+  }, 750)
+})
+
+option_Options.addEventListener('click', (e) => {
+  playMenuSound()
+  e.preventDefault()
+  setTimeout(() => {
+    window.location.href = 'src/pages/options.html';
+  }, 750)
+})
